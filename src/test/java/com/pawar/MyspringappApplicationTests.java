0750e5a8bcaa172083f60e3edd02533da8c493f9
Amol Pawar <pawaramol22@gmail.com>
2019-03-04 22:55:09 -0800

    Add templates, i18m config

--- src/test/java/com/pawar/MyspringappApplicationTests.java
+++ src/test/java/com/pawar/MyspringappApplicationTests.java
@@ -1,7 +1,10 @@
 package com.pawar;
 
+import com.pawar.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MyspringappApplicationTests {
 
+	@Autowired
+	private I18NService i18service;
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.h1";
+		Assert.assertEquals(expectedResult, i18service.getMessage(messageId));
 	}
 
 }
